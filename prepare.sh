5a40f460ed6fb4d6dfce9becc9b3a9daa6b7edcb
Piotr Ignatowski <pignatovsky@gmail.com>
2025-07-19 11:58:28 +0200

    Vim PlugInstall step as a batch part of prepare script.

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,9 @@ echo "Downloading Plug for vim..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "Installing plugins..."
+vim +PlugInstall +qa
+
 echo "Init and update of  Alacritty themes sumbodule"
 git submodule init
 git submodule update
