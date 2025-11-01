222a2afbd2ec8c9c0ba27ef9f7718c422b408ba9
Piotr Ignatowski <pignatovsky@gmail.com>
2025-11-01 17:11:24 +0100

    Better Iosevka variant

--- config/doom/config.el
+++ config/doom/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "Iosevka" :size 18))
+(setq doom-font (font-spec :family "Iosevka Term SS11" :size 18))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
